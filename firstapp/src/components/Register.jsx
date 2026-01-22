403085fb8d819ad9b86f751b397e8c5f5962b539
Ram-sadist <nagulavancharavisivaramteja@gmail.com>
2026-01-22 14:33:33 +0530

    navigation

--- firstapp/src/components/Register.jsx
+++ firstapp/src/components/Register.jsx
@@ -9,7 +9,14 @@ export default function Register() {
 
   function handleRegister(e){
     e.preventDefault()
-    console.log(e)
+    //console.log(e)
+    let newUser={name,email,mobile,password,address}
+    console.log(newUser)
+    setName("")
+    setEmail("")
+    setPassword("")
+    setMobile("")
+    setAddress("")
   }
 
   return (
@@ -19,26 +26,26 @@ export default function Register() {
           <h2>Register</h2>
           <div className='mb-3'>
               <label className="form-label">Name </label>
-              <input type="text" className="form-control" name="name"/>
+              <input type="text" className="form-control" name="name" value={name} onChange={(e)=>setName(e.target.value)}/>
             </div>
 
             <div className='mb-3'>
               <label className="form-label">Email </label>
-              <input type="email" className="form-control" name="email"/>
+              <input type="email" className="form-control" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
             </div>
             <div className='mb-3'>
               <label className="form-label">Password </label>
-              <input type="password" className="form-control" name="password"/>
+              <input type="password" className="form-control" name="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
             </div>
 
             <div className='mb-3'>
               <label className="form-label">Mobile Number</label>
-              <input type="text" className="form-control" name="mobile"/>
+              <input type="text" className="form-control" name="mobile" value={mobile} onChange={(e)=>setMobile(e.target.value)}/>
             </div>
 
             <div className='mb-3'>
               <label className="form-label">Address </label>
-              <input type="text" className="form-control" name="address"/>
+              <input type="text" className="form-control" name="address" value={address} onChange={(e)=>setAddress(e.target.value)}/>
             </div>
             <div className='mb-3'>
               <button className='btn btn-outline-success btn-lg'>Submit</button>
