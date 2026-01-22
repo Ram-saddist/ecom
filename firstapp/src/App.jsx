535ff2fe6b7b548e121dcaf58bdc2d4b69d6a1a0
Ram-sadist <nagulavancharavisivaramteja@gmail.com>
2026-01-22 09:57:49 +0530

    first commit

---
+++ firstapp/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from './components/Home.jsx'
+import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>      
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
