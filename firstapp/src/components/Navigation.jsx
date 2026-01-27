178aa06fc0131945b3df0bf40d339d6798eea01b
Ram-sadist <nagulavancharavisivaramteja@gmail.com>
2026-01-27 10:50:24 +0530

    nVIGtion updatex

--- firstapp/src/components/Navigation.jsx
+++ firstapp/src/components/Navigation.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Navigation() {
+  const navigate = useNavigate()
+  const userId = localStorage.getItem("userId")
+  const userRole = localStorage.getItem("role")
+  console.log(userRole)
+  function handleLogout(){
+    localStorage.removeItem("userId")
+    localStorage.removeItem("role")
+    navigate("/login")
+  }
   return (
     <div>
       <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+      {
+        userId ? (
+          <>
+            {userRole == "admin" ? (
+              <>
+              <Link to="/add-product">Add Product</Link> 
+                <Link onClick={handleLogout}>Logout</Link>
+
+              </>
+            ): 
+            (
+              <>
+                <Link to="/cart">Cart</Link>
+                <Link onClick={handleLogout}>Logout</Link>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )
+      }
     </div>
   )
 }
