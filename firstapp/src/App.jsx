d66dd13f3801fab77db92682a2daacc2addb28ec
Ram-sadist <nagulavancharavisivaramteja@gmail.com>
2026-01-28 11:57:42 +0530

    product, cart schema

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -14,7 +14,6 @@ export default function App() {
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/add-product" element={<AddProduct/>}/>
-
       </Routes>
     </BrowserRouter>
   )
